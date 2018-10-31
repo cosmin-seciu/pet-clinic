f2a8c45f97d25c4db388fb553f3031f7a51500b4
cosmin-seciu <cosmin.seciu9@gmail.com>
2018-10-31 15:19:11 +0200

    Added Bootstrap for Data. Closes #14

--- pet-clinic-data/src/main/java/spring/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/spring/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package spring.petclinic.services.map;
 
 import spring.petclinic.model.Pet;
-import spring.petclinic.services.CrudService;
+import spring.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long>{
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
@@ -13,9 +13,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements Crud
     }
 
     @Override
-    public Pet findById(Long id) {
-        return super.findById(id);
-    }
+    public Pet findById(Long id) { return super.findById(id); }
 
     @Override
     public Pet save(Pet object) {
