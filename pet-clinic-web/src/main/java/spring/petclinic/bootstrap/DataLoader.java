48b391e7aee5ff9f377fb3ca65803109503ecc36
cosmin-seciu <cosmin.seciu9@gmail.com>
2018-10-31 15:24:51 +0200

    Added Spring Config. Closes #22

--- pet-clinic-web/src/main/java/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/spring/petclinic/bootstrap/DataLoader.java
@@ -6,8 +6,6 @@ import spring.petclinic.model.Owner;
 import spring.petclinic.model.Vet;
 import spring.petclinic.services.OwnerService;
 import spring.petclinic.services.VetService;
-import spring.petclinic.services.map.OwnerServiceMap;
-import spring.petclinic.services.map.VetServiceMap;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
