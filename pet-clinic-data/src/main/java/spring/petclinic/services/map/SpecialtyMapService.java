dcf6c1ae77e8f76972fec7638f08ebd17aa99c6d
cosmin-seciu <cosmin.seciu9@gmail.com>
2018-11-06 15:58:48 +0200

    Added Spring Data JPA Service for Specialties. Closes #44

--- pet-clinic-data/src/main/java/spring/petclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/spring/petclinic/services/map/SpecialtyMapService.java
@@ -7,7 +7,7 @@ import spring.petclinic.services.SpecialtyService;
 import java.util.Set;
 
 @Service
-public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
 
     @Override
     public Set<Specialty> findAll() {
